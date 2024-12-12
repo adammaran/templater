1ea80b70b980732cdfc26a43662cb3bb31154f64
adammaran <adam.maran@united.group>
2024-12-12 22:37:26 +0100

    pulled main

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# templater
 Used for creating a specific CTA template
---
+++ android/app/src/main/java/com/templater/templater/MainActivity.java
@@ -0,0 +1,6 @@
+package com.templater.templater;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
